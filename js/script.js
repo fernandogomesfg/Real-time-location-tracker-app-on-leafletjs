d6ac8e050654206f93c1deceda7059b3509e2b7c
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-11-26 00:10:04 +0200

    chore: ultima actualizacao

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
 //mapa inicializado
-var map = L.map('map').setView([-25.953724, 32.588711], 12);
+var map = L.map('map').setView([-19.796382, 34.884487], 7);
 
 //camada osm
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 })
 osm.addTo(map);
+
+if (!navigator.geolocation) {
+    console.log("Your browser don't support geolocation feature!")
+} else {
+    setInterval(() => {
+        navigator.geolocation.getCurrentPosition(getPosition)
+    }, 5000)
+    
+}
+
+var marker, circle;
+function getPosition(position) {
+    // console.log(position)
+    var lat = position.coords.latitude
+    var long = position.coords.longitude
+    var accuracy = position.coords.accuracy
+
+    if (marker) {
+        map.removeLayer(marker)
+    }
+
+    if (circle) {
+        map.removeLayer(circle)
+    }
+
+    marker = L.marker([lat, long])
+    circle = L.circle([lat, long], {radius: accuracy})
+
+    var featureGroup = L.featureGroup([marker, circle]).addTo(map)
+
+    map.fitBounds(featureGroup.getBounds())
+
+    console.log('Latitude: ' + lat + ' Longitude: ' + long + ' Acuracia: ' + accuracy)
+}
